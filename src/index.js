33572277ab550be99e6f3713d19cc2a58320e600
Mateja97 <matejarkvc@gmail.com>
2022-04-16 16:15:04 +0200

    msg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React,{Fragment} from "react";
+import {
+  SafeAreaView,
+  View
+} from 'react-native';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+class Main extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <SafeAreaView style={{ flex:0, backgroundColor: 'red' }} />
+        <SafeAreaView style={{ flex:1, backgroundColor: 'gray' }}>
+          <View style={{ flex: 1, backgroundColor: 'white' }} />
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +31,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
